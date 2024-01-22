0ff61c19d6cd64e7537d6025da2c51806c0f2b2b
Sonny Kam <hkam0006@student.monash.edu>
2024-01-22 20:23:01 +0800

    Added global typography in theme file

--- splitbuddy/src/theme/theme.tsx
+++ splitbuddy/src/theme/theme.tsx
@@ -1,15 +1,22 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-    palette: {
-        mode: "dark",
-        background: {
-            default: '#1D1F26'
-        },
-        primary: {
-            main: '#ffea61'
-        }
+  palette: {
+    mode: "dark",
+    background: {
+      default: '#1D1F26'
+    },
+    primary: {
+      main: '#ffea61'
     }
+  },
+  typography: {
+    fontFamily: 'Lato, sans-serif',
+    button: {
+      textTransform: "unset",
+      fontWeight: 700
+    }
+  }
 })
 
 export default theme;
