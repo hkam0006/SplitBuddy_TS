33a9428ad567f4fa6ae64a6a70f289694acb60d1
Sonny Kam <hkam0006@student.monash.edu>
2024-01-25 20:52:24 +0800

    Added search functionality in the top bar component

--- splitbuddy/src/components/TopBar.tsx
+++ splitbuddy/src/components/TopBar.tsx
@@ -7,14 +7,34 @@ import { signOut } from "firebase/auth";
 import AppLogo from "./AppLogo"
 import { auth } from "../firebase";
 import theme from "../theme/theme";
+import { SetStateAction } from "react";
 
-const TopBar = () => {
+type GroupObject = {
+  name: string,
+  id: string
+}
+
+type TopBarProps = {
+  filteredBoards: GroupObject[],
+  setFilteredBoards: React.Dispatch<SetStateAction<GroupObject[]>>,
+  unfilteredBoards: GroupObject[],
+}
+
+const TopBar = ({ filteredBoards, setFilteredBoards, unfilteredBoards }: TopBarProps) => {
   const isXs: boolean = useMediaQuery(theme.breakpoints.only('xs'))
 
   async function handleLogout() {
     await signOut(auth)
   }
 
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    if (event.target.value.length === 0) {
+      return setFilteredBoards(unfilteredBoards)
+    }
+    const searchQuery = event.target.value.toLowerCase()
+    const arrayCopy = filteredBoards.filter((grp) => grp.name.toLowerCase().includes(searchQuery))
+    setFilteredBoards(arrayCopy)
+  }
 
   return (
     <AppBar position="static">
@@ -24,6 +44,7 @@ const TopBar = () => {
           variant="standard"
           size="medium"
           placeholder="Search..."
+          onChange={(e) => handleSearch(e)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
