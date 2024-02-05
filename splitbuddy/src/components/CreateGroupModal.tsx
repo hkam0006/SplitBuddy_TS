a86f804b070310e05bc3447871305a489dc6d1e1
Sonny Kam <hkam0006@student.monash.edu>
2024-02-06 01:47:49 +0800

    Added created group modal

--- splitbuddy/src/components/CreateGroupModal.tsx
+++ splitbuddy/src/components/CreateGroupModal.tsx
@@ -1,18 +1,61 @@
-import { Dialog, Stack, Typography, IconButton, TextField } from "@mui/material"
+import { Dialog, Stack, Typography, IconButton, TextField, Button, SelectChangeEvent, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
 import ModalHeader from "./ModalHeader"
-import CurrencySelect from "./utils/CurrencySelect"
+import { useState } from "react"
+import useApp, { CurrencyType } from "./hooks/useApp"
+import { create } from "domain"
+import useStore from "../store"
 
 type CreateGroupModal = {
   onClose: React.MouseEventHandler<HTMLButtonElement>
 }
 
+const currencies = [
+  "USD",
+  "AUD",
+  "EUR",
+  "YEN",
+  "WON",
+  "HKD"
+]
+
 const CreateGroupModal = ({ onClose }: CreateGroupModal) => {
+  const { createGroup } = useApp()
+  const { groups } = useStore()
+  const [groupName, setGroupName] = useState<string>("")
+  const [currency, setCurrency] = useState<CurrencyType>("USD")
+
+  async function handleSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    try {
+      await createGroup(groupName, currency)
+      console.log(groups)
+      onClose(event)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  function handleChange(event: SelectChangeEvent) {
+    setCurrency(event.target.value as CurrencyType)
+  }
   return (
     <Dialog open fullWidth maxWidth='xs' onClose={onClose}>
       <Stack p={3} spacing={2}>
         <ModalHeader onClose={onClose} />
-        <TextField label='Group Name' />
-        <CurrencySelect />
+        <TextField label='Group Name' onChange={(e) => setGroupName(e.target.value)} />
+        <FormControl>
+          <InputLabel>Currency</InputLabel>
+          <Select
+            label='Currency'
+            labelId="curr-select-label"
+            id="curr-select"
+            value={currency}
+            key={currency}
+            onChange={handleChange}
+          >
+            {currencies.map((curr) => <MenuItem value={curr} key={curr}>{curr}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <Button variant='contained' onClick={(e) => handleSubmit(e)}>Create Group</Button>
       </Stack>
     </Dialog>
   )
