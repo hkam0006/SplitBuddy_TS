554dd8277b593f8711c339146b1a7afe5f776975
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-02-10 20:03:13 +0800

    Removed hard coded modal header title

--- splitbuddy/src/components/ModalHeader.tsx
+++ splitbuddy/src/components/ModalHeader.tsx
@@ -2,14 +2,15 @@ import { IconButton, Stack, Typography } from "@mui/material"
 import CloseIcon from '@mui/icons-material/Close'
 
 type ModalHeader = {
-  onClose: React.MouseEventHandler<HTMLButtonElement>
+  onClose: React.MouseEventHandler<HTMLButtonElement>,
+  title: string,
 }
 
-const ModalHeader = ({ onClose }: ModalHeader) => {
+const ModalHeader = ({ onClose, title }: ModalHeader) => {
 
   return (
     <Stack direction='row' justifyContent='space-between' alignItems='center'>
-      <Typography variant='h5'>Add Expense Group</Typography>
+      <Typography variant='h5'>{title}</Typography>
       <IconButton onClick={onClose} size='small' ><CloseIcon /></IconButton>
     </Stack>
   )
