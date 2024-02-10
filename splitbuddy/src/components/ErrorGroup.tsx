09496e582364f5807f3c9708c8ff33d94dfd2e03
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-02-10 20:02:28 +0800

    Added error group component

---
+++ splitbuddy/src/components/ErrorGroup.tsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from "@mui/material"
+
+
+const ErrorGroup = () => {
+  return (
+    <Stack>
+      <Typography>Error! We could not find the board your looking for. Try again later</Typography>
+    </Stack>
+  )
+}
+
+export default ErrorGroup
